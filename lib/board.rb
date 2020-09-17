13664e1e274f2c1e2d027e7fb93b5146114b2928
adeeba reaz <adeebarivera@outlook.com>
2020-09-17 00:21:05 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,39 +1,51 @@
 
-class Board
-  attr_accessor :cells
-  def initialize
-    reset!
-  end
-
+class Board 
+  attr_accessor :cells 
+  
+  @@cells = [] 
+  
   def reset!
-    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  end
+    self.cells = Array.new(9, " ")
+  end 
+  
+  def initialize 
+    self.cells = Array.new(9, " ")
+  end 
+  
   def display
-    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
     puts "-----------"
-    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
     puts "-----------"
-    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+    puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
   end
-  def position(input)
-    input = input.to_i
-    cells[input - 1]
-  end
-  def full?
-    cells.none? {|cell| cell == " " || cell == nil}
-  end
+  
+  def position(user_input)  
+    self.cells[user_input.to_i-1] 
+  end 
+    
+  def update(position, player)
+      self.cells[position.to_i-1] = player.token
+  end 
+  #convert to board cells' array index 
+  #set it equal to the token
+  
+  def full? 
+    self.cells.all? {|cell| cell == "X" || cell == "O"} 
+  end 
+  
   def turn_count
-    cells.count {|cell| cell == "X" || cell == "O"}
-  end
-  def taken?(input)
-    position(input) == "X" || position(input) == "O"
-  end
-  def valid_move?(input)
-    input = input.to_i
-    !(taken?(input)) && input.between?(1, 9)
-  end
-  def update(input, player)
-    input = input.to_i
-    cells[input -1] = player.token
-  end
-end
+    self.cells.count("X") + self.cells.count("O") 
+  end 
+  
+  def taken?(position)  
+    #will return true or false for an individual position
+    self.cells[position.to_i-1] == "X" || 
+    self.cells[position.to_i-1] == "O" 
+  end 
+  
+  def valid_move?(position) 
+    !taken?(position) && position.to_i >0 && position.to_i <=9 
+  end 
+
+end 
